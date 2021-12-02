5dd7fcb0928b96db05a6a3ea49063c80b8883adb
Maria Olaru <molaru95@gmail.com>
2021-12-01 16:14:49 -0800

    update plot_montage()

--- plts/plot_montage.py
+++ plts/plot_montage.py
@@ -6,16 +6,30 @@ import proc.process_funcs as proc
 import plts.plot_funcs as plot
 
 def plot_montage(dir_name, labels):
-
     [msc, df_notes, gp] = preproc.preprocess_settings(dir_name)
     md = preproc.preprocess_data(dir_name, msc, gp) #separate fn b/c can take much longer time to process data
     msc = preproc.label_montage(msc, labels) #requires 1 label/montage session file
-    df =  preproc.link_data(msc, md) #create combined dataframe
     
-    [psds_500, psds_1000] = proc.convert_psd_montage(df)
+    #Plot contact pair combinations and conditions at downsampled rate
+    sr_ds = 500
+    [msc_ds, md_ds] = proc.downsample_data(msc, md, sr_ds)
     
-    plot.plot_PSD_montage_channels(psds_500, msc, gp, 500) 
-    plot.plot_PSD_montage_channels(psds_1000, msc, gp, 1000) 
+    df_linked_ds = preproc.link_data(msc_ds, md_ds, gp)
+    df_linked_ds = df_linked_ds[df_linked_ds['sr'] >= sr_ds]
     
-    plot.plot_PSD_montage_conditions(psds_500, msc, gp, 500)
-    plot.plot_PSD_montage_conditions(psds_1000, msc, gp, 1000)
+    psds_ds = proc.convert_psd_montage(df_linked_ds, sr_ds)
+    
+    plot.plot_PSD_montage_channels(psds_ds, msc_ds, sr_ds, gp) 
+    plot.plot_PSD_montage_conditions(psds_ds, msc_ds, sr_ds, gp)
+    
+    #Plot HFOs for high sampling rates
+    df_linked = preproc.link_data(msc, md, gp)
+    
+    sr_hfo = 1000
+    df_linked = df_linked[df_linked['sr'] == sr_hfo]
+    
+    psds_hfo = proc.convert_psd_montage(df_linked, sr_hfo)
+    
+    plot.plot_PSD_montage_channels(psds_hfo, msc, sr_hfo, gp) 
+    plot.plot_PSD_montage_conditions(psds_hfo, msc, sr_hfo, gp)
+
